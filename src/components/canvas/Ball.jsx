e5762921c9ce49df88184a498813c8e631288a27
abhi <abhi.nolyfe@gmail.com>
2023-06-23 20:37:17 +0530

    added functionality so that balls face the user when no interaction

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,23 +1,43 @@
-import React, { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
+import React, { Suspense, useRef, useState, useEffect } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { Decal, Float, OrbitControls, Preload, useTexture } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
 const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
+  const meshRef = useRef();
+  const [isInteracting, setIsInteracting] = useState(false);
+  const timeoutRef = useRef(null);
+
+  useEffect(() => {
+    const handleInteraction = () => {
+      setIsInteracting(true);
+      clearTimeout(timeoutRef.current);
+      timeoutRef.current = setTimeout(() => setIsInteracting(false), 1000);
+    };
+    window.addEventListener("mousemove", handleInteraction);
+    return () => window.removeEventListener("mousemove", handleInteraction);
+  }, []);
+
+  useFrame(({ camera }) => {
+    if (!isInteracting) {
+      meshRef.current.lookAt(camera.position);
+    }
+  });
 
   return (
     <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-      <ambientLight intensity={0.25} />
+      <ambientLight intensity={0.5} />
       <directionalLight position={[0, 0, 0.05]} />
-      <mesh castShadow receiveShadow scale={2.75}>
+      <mesh castShadow receiveShadow ref={meshRef} scale={2.75}>
         <icosahedronGeometry args={[1, 10]} />
-        <meshStandardMaterial 
+        <meshStandardMaterial
           color="#19001E"
           polygonOffset
           polygonOffsetFactor={-5}
           flatShading
+          roughness={0.85}
         />
         <Decal
           position={[0, 0, 1]}
@@ -26,8 +46,8 @@ const Ball = (props) => {
         />
       </mesh>
     </Float>
-  )
-}
+  );
+};
 
 const BallCanvas = ({ icon, name }) => {
   return (
@@ -49,8 +69,7 @@ const BallCanvas = ({ icon, name }) => {
         {name}
       </div>
     </>
-  )
-}
-
+  );
+};
 
 export default BallCanvas;
