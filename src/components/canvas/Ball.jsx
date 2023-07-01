6db3bbd6d81d22ea32148e111f1ee51d79c4ef12
abhi <abhi.nolyfe@gmail.com>
2023-07-01 05:32:22 +0530

    minor

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useRef, useState, useEffect } from "react";
+import { Suspense, useRef, useState, useEffect } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Decal, Float, OrbitControls, Preload, useTexture } from "@react-three/drei";
 import * as THREE from "three";
